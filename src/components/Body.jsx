1c7aa5879b1c3b86de7c1514a27a4bdbcd2d293b
Ronixa584 <asp15299@gmail.com>
2024-06-03 00:34:04 +0530

    Redesigned bill section and made filter sticky as per scrolling

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,189 @@
-import React, {useEffect } from "react";
-import { Link} from "react-router-dom";
+// import React, {useEffect } from "react";
+// import { Link} from "react-router-dom";
+// import ResturantCard from "./ResturantCard";
+// import Shimmer from "./Shimmer";
+// import { filterData } from "../utils/helper";
+// import useOnline from "../utils/useOnline";
+// import useRestaurantList from "../utils/useRestaurantList";
+// import NA from "./NA";
+// import MidCarousal from "./MidCarousal";
+// import { useCity } from "../utils/cityContext";
+// import banner1 from "../../public/banner1.jpg";
+
+// const Body = () => {
+//   const isOnline = useOnline();
+//   const { location } = useCity();
+
+//   //New one to get city name using context
+//   useEffect(() => {
+//     //console.log("Finally " + location); // This will log the city whenever it changes
+//   }, [location]); // Run this effect whenever city changes
+
+//   let selectedCity = location;
+  
+//   const [
+//     searchInput,
+//     setSearchInput,
+//     filteredRestaurants,
+//     setFilteredRestaurants,
+//     allRestaurants,
+//   ] = useRestaurantList({ selectedCity });
+  
+//   console.log(allRestaurants);
+
+//   if (!isOnline) {
+//     return <h1>Offline, Please check your internet connection</h1>;
+//   }
+
+//   if (!allRestaurants) {
+//     return <NA />;
+//   }
+
+//   const handleSearch = () => {
+//     const data = filterData(searchInput, allRestaurants);
+//     setFilteredRestaurants(data);
+//   };
+
+//   const handleKeyPress = (e) => {
+//     if (e.key === "Enter") {
+//       handleSearch();
+//     }
+//   };
+
+//       const handleFastDelivery = () => {
+//         const fastDeliveryOutput = allRestaurants.filter(
+//           (res) => res.info?.sla?.deliveryTime < 40
+//         );
+//         setFilteredRestaurants(fastDeliveryOutput);
+//       };
+
+//       // const handleVeg = () => {
+//       //   const vegOutput = allRestaurants.filter((res) => res.veg === true);
+//       //   setFilteredRestaurants(vegOutput);
+//       // };
+
+//       const handleRating = () => {
+//         const ratingOutput = allRestaurants.filter(
+//           (res) => Number(res.info?.avgRatingString) > 4.2
+//         );
+//         setFilteredRestaurants(ratingOutput);
+//       };
+
+//       const handleRange = (rangeKey) => {
+//         const rangeOutput = allRestaurants.filter((res) => {
+//           const cost = res.info.costForTwo.match(/\d+/g).map(Number)[0];
+//           if (rangeKey === 1) return cost >= 300 && cost <= 600;
+//           else if (rangeKey === 2) return cost < 300;
+//         });
+//         setFilteredRestaurants(rangeOutput);
+//       };
+
+//   return allRestaurants.length === 0 ? (
+//     <Shimmer />
+//   ) : (
+//     <div className="Body min-h-screen ">
+//       <MidCarousal />
+
+//       <div
+//         className="Bannerh-80"
+//         style={{
+//           backgroundImage: `url(${banner1})`,
+//           backgroundSize: "cover",
+//           backgroundPosition: "center",
+//           height: 350,
+//         }}
+//       >
+//         <div className="search-container p-5 flex flex-col md,sm:flex-row space-y-4 md,sm:space-y-0 md,sm:space-x-4 items-center">
+//           <div className="">
+//             <input
+//               type="text"
+//               className="search-btn p-2 m-2 text-black rounded-md shadow-xl transition w-full backdrop-blur-md"
+//               placeholder="Search Food Item ..."
+//               value={searchInput}
+//               onChange={(e) => setSearchInput(e.target.value)}
+//               onKeyPress={handleKeyPress}
+//             />
+//           </div>
+//           <div className="flex items-center backdrop-blur-md">
+//             <button
+//               className="search-btn p-2 m-2 text-white rounded-md shadow-xl"
+//               onClick={handleSearch}
+//             >
+//               Search
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="filters-items fixed top-0 w-full z-50 flex flex-wrap gap-4 justify-center my-4 px-4">
+//         <button
+//           className="filter-btn p-2 bg-gray-200 text-gray-800 rounded-md shadow-md hover:bg-gray-300 transition duration-200"
+//           onClick={handleSearch}
+//         >
+//           Reset
+//         </button>
+//         <button
+//           className="filter-btn p-2 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 transition duration-200"
+//           onClick={handleFastDelivery}
+//         >
+//           Fast Delivery
+//         </button>
+//         <button
+//           className="filter-btn p-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-200"
+//           onClick={handleRating}
+//         >
+//           Ratings 4.2+
+//         </button>
+//         <button
+//           className="filter-btn p-2 bg-yellow-500 text-white rounded-md shadow-md hover:bg-yellow-600 transition duration-200"
+//           onClick={() => handleRange(1)}
+//         >
+//           Rs. 300-Rs. 600
+//         </button>
+//         <button
+//           className="filter-btn p-2 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 transition duration-200"
+//           onClick={() => handleRange(2)}
+//         >
+//           Less than Rs. 300
+//         </button>
+//       </div>
+
+//       {/* Resturants loading */}
+//       <div className="resturantcard-list flex flex-wrap place-content-evenly">
+//         {filteredRestaurants.map((restaurant) => (
+//           <Link
+//             to={`/restaurant/${restaurant?.info?.id}`}
+//             key={restaurant?.info?.id}
+//           >
+//             <ResturantCard {...restaurant.info} />
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Body;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import ResturantCard from "./ResturantCard";
 import Shimmer from "./Shimmer";
 import { filterData } from "../utils/helper";
@@ -13,6 +197,7 @@ import banner1 from "../../public/banner1.jpg";
 const Body = () => {
   const isOnline = useOnline();
   const { location } = useCity();
+  const [isFixed, setIsFixed] = useState(false);
 
   //New one to get city name using context
   useEffect(() => {
@@ -20,7 +205,7 @@ const Body = () => {
   }, [location]); // Run this effect whenever city changes
 
   let selectedCity = location;
-  
+
   const [
     searchInput,
     setSearchInput,
@@ -28,7 +213,7 @@ const Body = () => {
     setFilteredRestaurants,
     allRestaurants,
   ] = useRestaurantList({ selectedCity });
-  
+
   console.log(allRestaurants);
 
   if (!isOnline) {
@@ -50,33 +235,44 @@ const Body = () => {
     }
   };
 
-      const handleFastDelivery = () => {
-        const fastDeliveryOutput = allRestaurants.filter(
-          (res) => res.info?.sla?.deliveryTime < 40
-        );
-        setFilteredRestaurants(fastDeliveryOutput);
-      };
+  const handleFastDelivery = () => {
+    const fastDeliveryOutput = allRestaurants.filter(
+      (res) => res.info?.sla?.deliveryTime < 40
+    );
+    setFilteredRestaurants(fastDeliveryOutput);
+  };
 
-      // const handleVeg = () => {
-      //   const vegOutput = allRestaurants.filter((res) => res.veg === true);
-      //   setFilteredRestaurants(vegOutput);
-      // };
+  const handleRating = () => {
+    const ratingOutput = allRestaurants.filter(
+      (res) => Number(res.info?.avgRatingString) > 4.2
+    );
+    setFilteredRestaurants(ratingOutput);
+  };
 
-      const handleRating = () => {
-        const ratingOutput = allRestaurants.filter(
-          (res) => Number(res.info?.avgRatingString) > 4.2
-        );
-        setFilteredRestaurants(ratingOutput);
-      };
+  const handleRange = (rangeKey) => {
+    const rangeOutput = allRestaurants.filter((res) => {
+      const cost = res.info.costForTwo.match(/\d+/g).map(Number)[0];
+      if (rangeKey === 1) return cost >= 300 && cost <= 600;
+      else if (rangeKey === 2) return cost < 300;
+    });
+    setFilteredRestaurants(rangeOutput);
+  };
 
-      const handleRange = (rangeKey) => {
-        const rangeOutput = allRestaurants.filter((res) => {
-          const cost = res.info.costForTwo.match(/\d+/g).map(Number)[0];
-          if (rangeKey === 1) return cost >= 300 && cost <= 600;
-          else if (rangeKey === 2) return cost < 300;
-        });
-        setFilteredRestaurants(rangeOutput);
-      };
+  useEffect(() => {
+    const handleScroll = () => {
+      const bannerHeight = document.getElementById("banner").clientHeight;
+      if (window.scrollY >= 2*bannerHeight+5) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return allRestaurants.length === 0 ? (
     <Shimmer />
@@ -85,7 +281,8 @@ const Body = () => {
       <MidCarousal />
 
       <div
-        className="Bannerh-80"
+        id="banner"
+        className="Banner h-80"
         style={{
           backgroundImage: `url(${banner1})`,
           backgroundSize: "cover",
@@ -112,54 +309,58 @@ const Body = () => {
               Search
             </button>
           </div>
-          {/* <div className="flex items-center">
-          <SearchableDropdown
-            options={cities.map((city) => city.name)}
-            value={cityName}
-            onChange={(e) => navigate(`/city/${e.target.value}`)}
-            onEnter={(searchTerm) => {
-              navigate(`/city/${searchTerm}`);
-            }}
-          />
-        </div> */}
         </div>
       </div>
 
-      <div className="filters-items flex flex-wrap gap-4 justify-center my-4 px-4">
-        <button
-          className="filter-btn p-2 bg-gray-200 text-gray-800 rounded-md shadow-md hover:bg-gray-300 transition duration-200"
-          onClick={handleSearch}
-        >
-          Reset
-        </button>
-        <button
-          className="filter-btn p-2 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 transition duration-200"
-          onClick={handleFastDelivery}
-        >
-          Fast Delivery
-        </button>
-        <button
-          className="filter-btn p-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-200"
-          onClick={handleRating}
-        >
-          Ratings 4.2+
-        </button>
-        <button
-          className="filter-btn p-2 bg-yellow-500 text-white rounded-md shadow-md hover:bg-yellow-600 transition duration-200"
-          onClick={() => handleRange(1)}
-        >
-          Rs. 300-Rs. 600
-        </button>
-        <button
-          className="filter-btn p-2 bg-red-500 text-white rounded-md shadow-md hover:bg-red-600 transition duration-200"
-          onClick={() => handleRange(2)}
-        >
-          Less than Rs. 300
-        </button>
+      <div
+        className={`filters-items w-full z-50 flex flex-wrap gap-4 justify-center  transition-all duration-200  ${
+          isFixed
+            ? "fixed top-0 bg-gradient-to-r from-green-100 via-white-800 to-red-100 w-full shadow-md"
+            : ""
+        }`}
+      >
+        <div className="flex flex-wrap gap-4 justify-center my-4 px-4">
+          <button
+            className="filter-btn p-2 bg-gray-200 text-gray-800 rounded-md shadow-md hover:bg-gray-300 transition duration-200"
+            onClick={handleSearch}
+          >
+            Reset
+          </button>
+          <button
+            className="filter-btn p-2 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 transition duration-200"
+            onClick={handleFastDelivery}
+          >
+            Fast Delivery
+          </button>
+          <button
+            className="filter-btn p-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-200"
+            onClick={handleRating}
+          >
+            Ratings 4.2+
+          </button>
+          <button
+            className="filter-btn p-2 bg-yellow-500 text-white rounded-md shadow-md hover:bg-yellow-600 transition duration-200"
+            onClick={() => handleRange(1)}
+          >
+            Rs. 300-Rs. 600
+          </button>
+          <button
+            className="filter-btn p-2 bg-red-500 text-white
+rounded-md shadow-md hover
+transition duration-200"
+            onClick={() => handleRange(2)}
+          >
+            Less than Rs. 300
+          </button>
+        </div>
       </div>
 
-      {/* Resturants loading */}
-      <div className="resturantcard-list flex flex-wrap place-content-evenly">
+      {/* Restaurants loading */}
+      <div
+        className={`restaurant-card-list flex flex-wrap place-content-evenly ${
+          isFixed ? "shadow-md mt-16" : ""
+        }`}
+      >
         {filteredRestaurants.map((restaurant) => (
           <Link
             to={`/restaurant/${restaurant?.info?.id}`}
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import FoodItem from "./FoodItem";
 import { clearCart } from "../utils/cartSlice";
 import cartImg from "../../public/7612.jpg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
@@ -13,20 +15,20 @@ const Cart = () => {
 
   return (
     <div
-      className={`min-h-screen ${
+      className={`min-h-screen  ${
         cartItems.length > 0
-          ? "bg-gradient-to-r from-green-200 via-white-800 to-red-300"
+          ? "bg-gradient-to-r from-green-200 via-white-800 to-red-300 "
           : "bg-white"
-      } p-4 flex flex-col items-center justify-center`}
+      } p-4 flex flex-col-reverse items-center justify-center `}
     >
       {cartItems.length > 0 ? (
-        <div className="w-full flex flex-col md:flex-row justify-between items-start">
-          <div className="w-full md:w-2/3 flex flex-col md:flex-row flex-wrap justify-center items-center">
+        <div className="w-full flex justify-between items-start">
+          {/* <div className="w-full lg:w-2/3 flex flex-col lg:flex-row flex-wrap justify-center items-center">
             {cartItems.map((item) => (
               <FoodItem key={item.id} {...item} />
             ))}
-          </div>
-          <div className="w-full md:w-1/3">
+          </div> */}
+          <div className="w-full  flex justify-center items-center">
             <Bill cartItems={cartItems} />
           </div>
         </div>
@@ -40,22 +42,22 @@ const Cart = () => {
           />
         </div>
       )}
-      {cartItems.length > 0 && (
+      {/* {cartItems.length > 0 && (
         <button
           onClick={handleClearCart}
           className="mt-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-700 transition-colors duration-300"
         >
           Clear Cart
         </button>
-      )}
+      )} */}
     </div>
   );
 };
 
 export default Cart;
 
-// import { useDispatch } from "react-redux";
 import { addItem, removeItem } from "../utils/cartSlice";
+import { IMG_CDN_URL, OFFER_LOGO_URL } from "../contants";
 
 const Bill = ({ cartItems }) => {
   const dispatch = useDispatch();
@@ -79,65 +81,174 @@ const Bill = ({ cartItems }) => {
   const totalPrice = calculateTotal();
   const discountPrice = totalPrice > 1000 ? totalPrice * 0.1 : 0; // Example discount logic
 
+  const formatPrice = (price) => {
+    return (Math.ceil(price * 100) / 100).toFixed(2);
+  };
+
   return (
     <div className="p-4 m-4 bg-white rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4">Bill Details</h2>
-      <div className="overflow-x-auto">
+      <div className="overflow-x-auto max-h-96">
         <table className="min-w-full bg-white">
-          <thead>
+          <thead className="sticky top-0 bg-gray-200 z-10">
             <tr>
-              <th className="py-2 px-4 bg-gray-200">Item</th>
-              <th className="py-2 px-4 bg-gray-200">Quantity</th>
-              <th className="py-2 px-4 bg-gray-200">Price</th>
+              <th className="py-2 px-4">Item</th>
+              <th className="py-2 px-4">Quantity</th>
+              <th className="py-2 px-4">Price</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="overflow-y-auto max-h-64">
             {cartItems.map((item) => (
               <tr key={item.id} className="border-t">
                 <td className="py-2 px-4">{item.name}</td>
-                <td className="py-2 px-4 text-center">
-                  <button
-                    onClick={() => handleRemoveItem(item)}
-                    className="px-2 py-1 bg-red-500 text-white rounded-md"
-                  >
-                    -
-                  </button>
-                  <span className="mx-2">{item.quantity}</span>
-                  <button
-                    onClick={() => handleAddItem(item)}
-                    className="px-2 py-1 bg-green-500 text-white rounded-md"
-                  >
-                    +
-                  </button>
-                </td>
+                <div className="relative mt-4">
+                  <img
+                    src={IMG_CDN_URL + item?.imageId}
+                    className="w-full md:w-48 h-32 object-cover rounded-lg"
+                  />
+                  <td className="absolute bottom-1 right-1/4 flex space-x-2">
+                    <div className="rounded-full flex justify-center items-center">
+                      <button
+                        onClick={() => handleRemoveItem(item)}
+                        className="bg-red-500 text-white py-1 px-3 hover:bg-red-600 transition duration-200 flex justify-center items-center"
+                      >
+                        -
+                      </button>
+                      <span className="bg-white text-black py-1 px-3 flex justify-center items-center">
+                        {item.quantity}
+                      </span>
+                      <button
+                        onClick={() => handleAddItem(item)}
+                        className="bg-green-500 text-white py-1 px-3 hover:bg-green-600 transition duration-200 flex justify-center items-center"
+                      >
+                        +
+                      </button>
+                    </div>
+                  </td>
+                </div>
                 <td className="py-2 px-4 text-right">
-                  {(isNaN(item.price) ? 399 : item.price)} Rs
+                  {isNaN(item.price) ? 399 : item.price / 100} Rs
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <div className="border-t pt-4 mt-4">
+      <div className="sticky bg-white pt-4 mt-4 border-t">
         <div className="flex justify-between">
           <span className="font-semibold">Total Price:</span>
-          <span>{totalPrice / 100} Rs</span>
+          <span>{formatPrice(totalPrice / 100)} Rs</span>
         </div>
         <div className="flex justify-between">
           <span className="font-semibold">Discount:</span>
-          <span>{discountPrice / 100} Rs</span>
+          <span>{formatPrice(discountPrice / 100)} Rs</span>
         </div>
         <div className="flex justify-between font-bold text-lg mt-2">
           <span>Final Price:</span>
-          <span>{(totalPrice - discountPrice) / 100} Rs</span>
+          <span>{formatPrice((totalPrice - discountPrice) / 100)} Rs</span>
         </div>
+        <button className="mt-4 w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-700 transition-colors duration-300">
+          Proceed to Payment
+        </button>
       </div>
-      <button className="mt-4 w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-700 transition-colors duration-300">
-        Proceed to Payment
-      </button>
     </div>
   );
 };
 
 // export default Bill;
 
+//Note
+//Need to fix the 399 issuse during total in bill
+
+//OLD
+// const Bill = ({ cartItems }) => {
+//   const dispatch = useDispatch();
+
+//   const handleAddItem = (item) => {
+//     dispatch(addItem(item));
+//   };
+
+//   const handleRemoveItem = (item) => {
+//     dispatch(removeItem(item));
+//   };
+
+//   const calculateTotal = () => {
+//     return cartItems.reduce(
+//       (total, item) =>
+//         total + (isNaN(item.price) ? 399 : item.price) * item.quantity,
+//       0
+//     );
+//   };
+
+//   const totalPrice = calculateTotal();
+//   const discountPrice = totalPrice > 1000 ? totalPrice * 0.1 : 0; // Example discount logic
+
+//   return (
+//     <div className="p-4 m-4 bg-white rounded-lg shadow-lg">
+//       <h2 className="text-2xl font-bold mb-4">Bill Details</h2>
+//       <div className="overflow-x-auto">
+//         <table className="min-w-full bg-white">
+//           <thead>
+//             <tr>
+//               <th className="py-2 px-4 bg-gray-200">Item</th>
+//               <th className="py-2 px-4 bg-gray-200">Quantity</th>
+//               <th className="py-2 px-4 bg-gray-200">Price</th>
+//             </tr>
+//           </thead>
+//           <tbody className="">
+//             {cartItems.map((item) => (
+//               <tr key={item.id} className="border-t">
+//                 <td className="py-2 px-4">{item.name}</td>
+//                 <div className="relative mt-4">
+//                   <img
+//                     src={IMG_CDN_URL + item?.imageId}
+//                     className="w-full md:w-48 h-32 object-cover rounded-lg"
+//                   />
+//                   <td className="absolute bottom-1 right-1/4 flex space-x-2">
+//                     <div className="rounded-full flex justify-center items-center">
+//                       <button
+//                         onClick={() => handleRemoveItem(item)}
+//                         className="bg-red-500 text-white py-1 px-3 hover:bg-red-600 transition duration-200 flex justify-center items-center"
+//                       >
+//                         -
+//                       </button>
+//                       <span className="bg-white text-black py-1 px-3 flex justify-center items-center">
+//                         {item.quantity}
+//                       </span>
+//                       <button
+//                         onClick={() => handleAddItem(item)}
+//                         className="bg-green-500 text-white py-1 px-3 hover:bg-green-600 transition duration-200 flex justify-center items-center"
+//                       >
+//                         +
+//                       </button>
+//                     </div>
+//                   </td>
+//                 </div>
+//                 <td className="py-2 px-4 text-right">
+//                   {isNaN(item.price) ? 399 : item.price / 100} Rs
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//       <div className="border-t pt-4 mt-4">
+//         <div className="flex justify-between">
+//           <span className="font-semibold">Total Price:</span>
+//           <span>{totalPrice / 100} Rs</span>
+//         </div>
+//         <div className="flex justify-between">
+//           <span className="font-semibold">Discount:</span>
+//           <span>{discountPrice / 100} Rs</span>
+//         </div>
+//         <div className="flex justify-between font-bold text-lg mt-2">
+//           <span>Final Price:</span>
+//           <span>{(totalPrice - discountPrice) / 100} Rs</span>
+//         </div>
+//       </div>
+//       <button className="mt-4 w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-700 transition-colors duration-300">
+//         Proceed to Payment
+//       </button>
+//     </div>
+//   );
+// };
